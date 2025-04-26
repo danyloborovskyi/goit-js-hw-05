6504829d72d566a6f104de78557577b71b0d71ca
Danylo Borovskyi <danilborovskii199911@gmail.com>
2025-04-26 18:43:31 +0300

    hometask is done

--- js/task-3.js
+++ js/task-3.js
@@ -1,38 +1,80 @@
-const profile = {
-    username: "Jacob",
-    playTime: 300,
-    changeUsername(newName) {
-        if (this.username !== newName) {
-            this.username = newName;
-        }
-    },
-    updatePlayTime(hours) {
-        if (this.playTime !== hours) {
-            this.playTime += hours;
-        }
-    },
-    getInfo() {
-        return `${this.username} has ${this.playTime} active hours!`;
-    },
+const sortByDescendingFriendCount = (users) => {
+    return users.toSorted((a, b) => b.friends.length - a.friends.length);
 };
 
-console.log(profile.getInfo()); 
-profile.changeUsername("Marco");
-
-console.log(profile.getInfo());
-
-profile.updatePlayTime(20);
-console.log(profile.getInfo());
-
-// const products = [
-//   { name: "Apple", price: 10, quantity: 3 },
-//   { name: "Banana", price: 5, quantity: 7 },
-//   { name: "Orange", price: 8, quantity: 4 },
-//   { name: "Mango", price: 15, quantity: 2 }
-// ];
-
-// const totalSum = products.reduce((sum, product) => {
-//     return sum + (product.price * product.quantity)
-// }, 0);
-
-// console.log(totalSum);
+console.log(
+  sortByDescendingFriendCount([
+    {
+      name: "Moore Hensley",
+      friends: ["Sharron Pace"],
+      gender: "male"
+    },
+    {
+      name: "Sharlene Bush",
+      friends: ["Briana Decker", "Sharron Pace"],
+      gender: "female"
+    },
+    {
+      name: "Ross Vazquez",
+      friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+      gender: "male"
+    },
+    {
+      name: "Elma Head",
+      friends: ["Goldie Gentry", "Aisha Tran"],
+      gender: "female"
+    },
+    {
+      name: "Carey Barr",
+      friends: ["Jordan Sampson", "Eddie Strong"],
+      gender: "male"
+    },
+    {
+      name: "Blackburn Dotson",
+      friends: ["Jacklyn Lucas", "Linda Chapman"],
+      gender: "male"
+    },
+    {
+      name: "Sheree Anthony",
+      friends: ["Goldie Gentry", "Briana Decker"],
+      gender: "female"
+    }
+  ])
+);
+// [
+//   {
+//     name: "Ross Vazquez",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//     gender: "male"
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//     gender: "female"
+//   },
+//   {
+//     name: "Elma Head",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//     gender: "female"
+//   },
+//   {
+//     name: "Carey Barr",
+//     friends: ["Jordan Sampson", "Eddie Strong"],
+//     gender: "male"
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     friends: ["Jacklyn Lucas", "Linda Chapman"],
+//     gender: "male"
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//     gender: "female"
+//   },
+//   {
+//     name: "Moore Hensley",
+//     friends: ["Sharron Pace"],
+//     gender: "male"
+//   }
+// ]
